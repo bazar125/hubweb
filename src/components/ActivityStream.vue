d6f44570c7ecf8d7dfd2dc5fa2858d1e64b02525
qqc <qqc@dev.null>
2017-09-12 11:23:24 +0100

    Asd

--- src/components/ActivityStream.vue
+++ src/components/ActivityStream.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="activity-stream d-flex flex-column justify-content-start align-items-center">
     <span class="master-title">Activity Stream</span>
-    <div v-for="activity in activities" :key="activity"
-      class="activity-stream-row d-flex flex-column justify-content-start align-items-center">
-      <span class="txt-title">New Citation</span>
+    <div v-for="activity in activities" :key="activity" class="activity-stream-row d-flex flex-column justify-content-start align-items-center">
       <div class="d-flex justify-content-start align-items-center" style="width: 100%;">
-        <icon name="location-arrow" class="icon-small"></icon>
-        <span class="txt-address">Murtala Muhammad Way, 31212 Lagos</span>
+        <icon name="pencil-square" class="icon-small"></icon>
+        <span class="txt-title">{{activity.location ? 'New Citation' : 'New Collision'}}</span>
       </div>
       <div class="d-flex justify-content-start align-items-center" style="width: 100%;">
-        <icon name="clock-o" class="icon-small"></icon>
+        <icon name="location-arrow" class="icon-small"></icon>
+        <span class="txt-address">{{activity.location ? activity.location : (activity.address ? activity.address : 'Unknown address') }}</span>
+      </div>
+      <div class="d-flex justify-content-start align-items-center" style="width: 100%;">
+        <icon name="clock-o" class="icon-small" style="margin-right: 7px;"></icon>
         <span class="txt-timeago">2 hours ago</span>
         <b-btn class="ml-auto btn-primary btn-view" size="sm">View</b-btn>
       </div>
@@ -19,14 +21,41 @@
 </template>
 
 <script>
+import * as Firebase from 'firebase';
+import TablePageLoader from '@/services/TablePageLoader';
+
+const pageLoader = new TablePageLoader('citation,collision');
+const ref = Firebase.database().ref();
+
 export default {
   name: 'ActivityStream',
   components: {
   },
   data() {
     return {
-      activities: [{}],
+      activities: [],
     };
+  },
+  mounted() {
+    pageLoader.load(1).then((page) => {
+      // this.activities = this.processRows(page.items);
+      this.activities = page.items;
+      this.totalRows = page.totalRows;
+    });
+
+    ref.child('citations').on('child_added').then(() => {
+      pageLoader.load(1).then((page) => {
+        this.activities = this.processRows(page.items);
+        this.totalRows = page.totalRows;
+      });
+    });
+
+    ref.child('collisions').on('child_added').then(() => {
+      pageLoader.load(1).then((page) => {
+        this.activities = this.processRows(page.items);
+        this.totalRows = page.totalRows;
+      });
+    });
   },
 };
 </script>
@@ -36,31 +65,30 @@ export default {
   width: 200px;
   min-width: 200px;
   z-index: 99;
-  box-shadow: 4px 8px 8px 2px rgba(0,0,0,0.3);
+  box-shadow: 4px 8px 8px 2px rgba(0, 0, 0, 0.3);
   overflow-y: auto;
 }
 
 .txt-title {
   font-weight: 600;
   font-size: 12px;
-  padding-left: 19px;
   width: 100%;
   text-align: start;
 }
 
 .txt-timeago {
-  font-size: 10px;
+  font-size: 9px;
 }
 
 .txt-address {
-  font-size: 10px;
+  font-size: 9px;
   width: 100%;
   text-align: start;
 }
 
 .activity-stream-row {
   width: 100%;
-  padding: 10px;
+  padding: 8px;
   border-bottom: 1px solid #DF90B8;
 }
 
@@ -72,6 +100,7 @@ export default {
   font-size: 10px;
   cursor: pointer;
   border-color: #DF90B8;
+  margin-top: 3px;
 }
 
 .icon-small {
