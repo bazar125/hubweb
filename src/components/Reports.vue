97f4776c3a1a44db4868bc4d264b496fc38c30c6
qqc <qqc@dev.null>
2017-09-28 00:34:47 +0100

    asd

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -4,58 +4,42 @@
       <div class="recorded-events-card d-flex justify-content-center align-items-center">
         <base-card :hidePlaceholder="true" style="flex: 1; height: 100%;">
           <div class="recorded-events-container d-flex flex-column justify-content-start align-items-center">
-            <span class="chart-title">Recorded Events (Monthly)</span>
-            <recorded-events-chart :width="400" :height="250" class="recorded-events-chart"></recorded-events-chart>
+            <span class="chart-title">Events This Month</span>
+            <recorded-events-chart :width="380" :height="250" class="recorded-events-chart"></recorded-events-chart>
           </div>
         </base-card>
       </div>
       <div class="revenue-card d-flex justify-content-center align-items-center">
         <base-card :hidePlaceholder="true" style="flex: 1; height: 100%;">
           <div class="revenue-container d-flex flex-column justify-content-start align-items-center">
-            <span class="chart-title">Revenue (Monthly)</span>
-            <revenue-chart :width="400" :height="250" class="revenue-chart"></revenue-chart>
+            <span class="chart-title">Revenue</span>
+            <revenue-chart :width="380" :height="250" class="revenue-chart"></revenue-chart>
           </div>
         </base-card>
       </div>
     </div>
     <div class="bottom-container d-flex justify-content-start align-items-start">
-      <div class="overdue-card d-flex justify-content-center align-items-center">
-        <base-card :hidePlaceholder="true" style="flex: 1; height: 100%;">
-          <div class="overdue-container d-flex flex-column justify-content-start align-items-start">
-            <div class="d-flex flex-column justify-content-start align-items-start">
-              <span class="chart-title">Payments Overdue</span>
-              <div class="action-btn-container d-flex justify-content-start align-items-start" style="margin-bottom: 10px;">
-                <b-btn class="action-btn" size="sm">
-                  <icon name="fire"></icon>
-                </b-btn>
-                <b-btn class="action-btn" size="sm">
-                  <icon name="user"></icon>
-                </b-btn>
-                <b-btn class="action-btn" size="sm">
-                  <icon name="line-chart"></icon>
-                </b-btn>
-              </div>
-              <div class="action-btn-container d-flex justify-content-start align-items-start">
-                <b-btn class="action-btn" size="sm">
-                  <icon name="fire"></icon>
-                </b-btn>
-                <b-btn class="action-btn" size="sm">
-                  <icon name="user"></icon>
-                </b-btn>
-                <b-btn class="action-btn" size="sm">
-                  <icon name="line-chart"></icon>
-                </b-btn>
-              </div>
-
-              <div class="d-flex justify-content-center align-items-center">
-                
-              </div>              
-
-            </div>
-            <!-- <revenue-chart :width="400" :height="40" class="revenue-chart"></revenue-chart> -->
+      <base-card :hidePlaceholder="true">
+        <div class="overdue-container d-flex flex-column justify-content-start align-items-start">
+          <div class="d-flex align-items-start" style="width: 100%;">
+            <span class="chart-title mr-auto">Payments Overdue</span>
+            <span style="padding-right: 10px; margin-right: 10px; font-size: 13px; border-right: 1px solid #ececec;">
+              <span class="chart-title">Overdue:</span> 231</span>
+            <span style="font-size: 13px;">
+              <span class="chart-title">Total:</span> ₦250,000</span>
           </div>
-        </base-card>
-      </div>
+          <div class="overdue-list-container d-flex justify-content-start align-items-center">
+            <div class="overdue-list d-flex justify-content-start align-items-center">
+              <div v-for="payment in paymentsOverdue" :key="payment" class="overdue-item d-flex justify-content-center align-items-center">
+                <b-card class="payment-card" title="₦10,000" sub-title="GWA456ASD">
+                  <span class="text-overdue">5 days overdue</span>
+                </b-card>
+              </div>
+            </div>
+          </div>
+          <!-- <revenue-chart :width="400" :height="40" class="revenue-chart"></revenue-chart> -->
+        </div>
+      </base-card>
     </div>
   </div>
 </template>
@@ -74,6 +58,7 @@ export default {
   },
   data() {
     return {
+      paymentsOverdue: ['', '', '', '', '', '', ''],
     };
   },
 };
@@ -81,14 +66,13 @@ export default {
 
 <style scoped>
 .reports {
-  width: 100%;
+  width: calc(100% - 350px);
   padding: 20px;
 }
 
 .charts-container {
   width: 100%;
 }
-
 
 /* 
 .recorded-events-container {
@@ -118,14 +102,62 @@ export default {
 .overdue-container {
   height: 100%;
   flex: 1;
+  overflow: hidden;
+}
+
+.overdue-list-root {
+  overflow: hidden;
+}
+
+.overdue-list-container {
+  height: 100%;
+  max-width: 100%;
+  overflow-x: auto;
+  /* overflow-y: hidden; */
+  white-space: nowrap;
+}
+
+.overdue-list {
+  height: 100%;
 }
 
 .overdue-card {
-  flex: 1;
+  width: auto;
+  /* flex: none; */
   height: 100%;
   padding-top: 10px;
   /* height: 400px; */
 }
+
+.overdue-item {
+  /* display: inline-block; */
+  flex: 1;
+  /* width: 200px; */
+  height: 100%;
+}
+
+.overdue-item:not(:last-child) {
+  padding-right: 20px;
+}
+
+.payment-card {
+  background-color: #c62828;
+  color: white;
+  cursor: pointer;
+  box-shadow: none;
+  transition: all .15s ease-in-out;
+  z-index: 3;
+}
+
+.payment-card:hover {
+  background-color: #d52d2d;
+  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  transition: all .15s ease-in-out;
+}
+
+/* .overdue-item:not(:last-child) {
+  border-right: 1px solid #ececec;
+} */
 
 .recorded-events-chart {
   flex: 1;
@@ -144,10 +176,31 @@ export default {
 .bottom-container {
   width: 100%;
   flex: 1;
+  padding-top: 10px;
 }
 
 .action-btn:not(:last-child) {
   margin-right: 10px;
 }
 
+.text-overdue {
+  font-size: 10px;
+  user-select: none;
+}
+</style>
+
+<style>
+.payment-card .card-block {
+  padding: 10px;
+}
+.payment-card .card-title {
+  font-weight: 300;
+  user-select: none;
+}
+.payment-card .card-subtitle {
+  font-weight: 600;
+  color: white !important;
+  margin-bottom: 0px !important;
+  user-select: none;
+}
 </style>
