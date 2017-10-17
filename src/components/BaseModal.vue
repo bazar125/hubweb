fdcf50cff808f0c59aab2321818af4fbdbb89cb6
qqc <qqc@dev.null>
2017-10-17 02:15:21 +0100

    asd

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -2,7 +2,9 @@
   <div class="driver-modal d-flex flex-column justify-content-center align-items-start">
     <div :class="{'yellow-heading': isWarning}" class="modal-heading d-flex justify-content-start align-items-center">
       <span class="txt-title">{{title}}</span>
-      <span v-if="this.type === 'driver' && this.data.status === 'Unverified'" :class="{'reference-nav-hidden': hideNavigation}" class="ml-auto txt-reference">Unverified <b-btn @click="clickVerify()" size="sm" class="btn-verify">Verify</b-btn></span>
+      <span v-if="this.type === 'driver' && this.data.status === 'Unverified'" :class="{'reference-nav-hidden': hideNavigation}" class="ml-auto txt-reference">Manually Added
+        <b-btn @click="clickVerify()" size="sm" class="btn-verify">VERIFY</b-btn>
+      </span>
       <span v-if="this.type === 'citation' || this.type === 'collision'" :class="{'reference-nav-hidden': hideNavigation}" class="ml-auto txt-reference">Ref: {{data.location ? data.paymentReference : data.reference}}</span>
       <div v-if="!hideNavigation" @click="clickNewTab()" class="ml-auto heading-btn-container d-flex justify-content-start align-items-center">
         <span class="txt-heading">open in new tab</span>
@@ -17,17 +19,17 @@
     </div>
     <!-- <modal-image-section :editBtnTitle="editBtnTitle" :auditBtnTitle="auditBtnTitle" @clickEdit="toggleEdit()" @clickAuditHistory="toggleAudit()" :type="type" :data="data"></modal-image-section> -->
 
-    <div v-if="showAudit" class="audit-root-container d-flex justify-content-start align-items-start">
+    <div v-if="showAudit && !showVerify" class="audit-root-container d-flex justify-content-start align-items-start">
       <modal-audit-section :record-type="type" :data="data"></modal-audit-section>
     </div>
-    <div v-else-if="showEdit" class="edit-root-container d-flex justify-content-start align-items-start">
+    <div v-else-if="showEdit && !showVerify" class="edit-root-container d-flex justify-content-start align-items-start">
       <modal-edit-section :name="type" :data="data"></modal-edit-section>
     </div>
-    <div v-else class="data-root-container d-flex justify-content-start align-items-start">
+    <div v-else-if="!showVerify" class="data-root-container d-flex justify-content-start align-items-start">
       <slot name="main" :data="data ? data : {}"></slot>
     </div>
 
-    <div v-if="!showEdit && this.type === 'citation' || this.type === 'collision'" class="driver-vehicle-container d-flex justify-content-center align-items-start">
+    <div v-if="!showEdit && !showVerify && this.type === 'citation' || this.type === 'collision'" class="driver-vehicle-container d-flex justify-content-center align-items-start">
       <div class="d-flex" style="flex: 0.5; height: 100%;">
         <modal-data-drivers :drivers="drivers" @viewDriver="viewDriver"></modal-data-drivers>
       </div>
@@ -36,7 +38,7 @@
       </div>
     </div>
 
-    <div v-if="!showEdit && this.type === 'collision'" class="driver-vehicle-container d-flex justify-content-center align-items-start">
+    <div v-if="!showEdit && !showVerify && this.type === 'collision'" class="driver-vehicle-container d-flex justify-content-center align-items-start">
       <div v-if="this.perpetrators.count > 0" class="d-flex" style="flex: 0.5; height: 100%;">
         <modal-data-drivers title="Perpetrators" :drivers="perpetrators" @viewDriver="viewDriver"></modal-data-drivers>
       </div>
@@ -45,19 +47,43 @@
       </div>
     </div>
 
-    <div v-if="!showEdit && this.type === 'collision' && this.pedestrians.count > 0" class="driver-vehicle-container d-flex justify-content-center align-items-start">
+    <div v-if="!showEdit && !showVerify && this.type === 'collision' && this.pedestrians.count > 0" class="driver-vehicle-container d-flex justify-content-center align-items-start">
       <div class="d-flex" style="flex: 0.5; height: 100%;">
         <modal-data-drivers title="Pedestrians" :drivers="pedestrians" @viewDriver="viewDriver"></modal-data-drivers>
       </div>
     </div>
 
-    <span v-if="!showEdit && (this.type === 'citation' && this.data.completionStatus !== 'Warning')" class="txt-timeleft">There are 12 days left to pay this citation</span>
+    <span v-if="!showEdit && !showVerify && (this.type === 'citation' && this.data.completionStatus !== 'Warning')" class="txt-timeleft">There are 12 days left to pay this citation</span>
 
-    <div :class="{'timeleft-hidden': showEdit || this.type === 'citation' && this.data.completionStatus === 'Warning' || this.type === 'collision' || this.type === 'driver' }" class="action-container d-flex justify-content-end align-items-center">
-      <base-btn @click="toggleEdit()" :class="{'btn-back': showEdit}" class="btn-delete" :text="editBtnTitle" :icon="editBtnIcon"></base-btn>
-      <base-btn v-if="!showEdit" @click="clickPrint()" class="btn-print" text="Print" icon="print"></base-btn>
-      <base-btn v-if="!showEdit" :class="{'btn-disabled': !userHasEmail}" @click="clickEmail()" class="btn-email" text="Email" icon="envelope-o"></base-btn>
-      <base-btn v-if="!showEdit" @click="clickPdf()" class="btn-pdf" text="PDF" icon="file-pdf-o"></base-btn>
+    <div v-if="showVerify && this.type === 'driver'" class="driver-vehicle-container d-flex flex-column justify-content-center align-items-center">
+      <div class="d-flex flex-column justify-content-start align-items-start" style="margin-top: 10px; margin-bottom: 10px;">
+        <span>
+          <b>Step 1:</b> Select photo scan from PC</span>
+        <div class="d-flex">
+          <input style="display: none;" ref="stepOneFile" type="file" @change="addFileOne($event)">
+          <b-form-input :disabled="true" class="name-file" v-model="fileOneName" size="sm" type="text"></b-form-input>
+          <base-btn @click="clickBrowseOne()" text="Browse" class="btn-browse"></base-btn>
+        </div>
+      </div>
+
+      <div class="d-flex flex-column justify-content-start align-items-start">
+        <span>
+          <b>Step 2:</b> Select proof of address scan from PC</span>
+        <div class="d-flex">
+          <input style="display: none;" ref="stepTwoFile" type="file" @change="addFileTwo($event)">
+          <b-form-input :disabled="true" class="name-file" v-model="fileTwoName" size="sm" type="text"></b-form-input>
+          <base-btn @click="clickBrowseTwo()" text="Browse" class="btn-browse"></base-btn>
+        </div>
+      </div>
+
+      <base-btn :class="{'btn-disabled': !allowSubmitVerification}" @click="clickSubmitVerification()" text="Submit Verification" class="mx-auto btn-submit-verification"></base-btn>
+    </div>
+
+    <div :class="{'timeleft-hidden': showEdit || showVerify || this.type === 'citation' && this.data.completionStatus === 'Warning' || this.type === 'collision' || this.type === 'driver' }" class="action-container d-flex justify-content-end align-items-center">
+      <base-btn @click="toggleEdit()" :class="{'btn-back': showEdit || showVerify}" class="btn-delete" :text="editBtnTitle" :icon="editBtnIcon"></base-btn>
+      <base-btn v-if="!showEdit && !showVerify" @click="clickPrint()" class="btn-print" text="Print" icon="print"></base-btn>
+      <base-btn v-if="!showEdit && !showVerify" :class="{'btn-disabled': !userHasEmail}" @click="clickEmail()" class="btn-email" text="Email" icon="envelope-o"></base-btn>
+      <base-btn v-if="!showEdit && !showVerify" @click="clickPdf()" class="btn-pdf" text="PDF" icon="file-pdf-o"></base-btn>
     </div>
   </div>
 </template>
@@ -70,6 +96,7 @@ import ModalDataDrivers from '@/components/ModalDataDrivers';
 import ModalDataVehicles from '@/components/ModalDataVehicles';
 // import TablePageLoader from '@/services/TablePageLoader';
 import BaseBtn from '@/components/BaseBtn';
+import * as Firebase from 'firebase';
 import html2canvas from 'html2canvas';
 import * as JsPDF from 'jspdf';
 import printJS from 'print-js';
@@ -91,10 +118,15 @@ export default {
     return {
       showAudit: false,
       showEdit: false,
+      showVerify: false,
       auditBtnTitle: 'History',
       editBtnTitle: this.type === 'citation' ? 'Delete' : 'Edit',
       editBtnIcon: this.type === 'citation' ? 'trash-o' : 'pencil',
       userHasEmail: true,
+      fileOne: '',
+      fileTwo: '',
+      fileOneName: '',
+      fileTwoName: '',
     };
   },
   computed: {
@@ -143,11 +175,31 @@ export default {
 
       return this.data.vehicles;
     },
+    allowSubmitVerification() {
+      if (this.fileOne && this.fileTwo) {
+        return true;
+      }
+      return false;
+    },
   },
   mounted() {
     this.userHasEmail = true;
   },
   methods: {
+    addFileOne(event) {
+      this.fileOne = event.target.files[0];
+      this.fileOneName = this.fileOne.name;
+    },
+    addFileTwo(event) {
+      this.fileTwo = event.target.files[0];
+      this.fileTwoName = this.fileTwo.name;
+    },
+    clickBrowseOne() {
+      this.$refs.stepOneFile.click();
+    },
+    clickBrowseTwo() {
+      this.$refs.stepTwoFile.click();
+    },
     toggleAudit() {
       this.showAudit = !this.showAudit;
       this.showEdit = false;
@@ -167,9 +219,10 @@ export default {
     },
     toggleEdit() {
       this.showEdit = !this.showEdit;
+      this.showVerify = false;
       this.showAudit = false;
 
-      if (this.showEdit) {
+      if (this.showEdit || this.showVerify) {
         this.editBtnTitle = 'Back';
         this.editBtnIcon = 'angle-left';
       } else if (this.type === 'citation') {
@@ -271,6 +324,39 @@ export default {
         const typeCapital = this.type.charAt(0).toUpperCase() + this.type.slice(1);
         doc.save(`${typeCapital}-${ref}.pdf`);
       });
+    },
+    clickVerify() {
+      this.editBtnTitle = 'Back';
+      this.editBtnIcon = 'angle-left';
+      this.showEdit = true;
+      this.showVerify = true;
+    },
+    clickSubmitVerification() {
+      if (!this.stepOneFile || !this.stepTwoFile || !this.data.$id) {
+        return;
+      }
+      const photoGuid = this.getGuid();
+      const addressGuid = this.GetGuid();
+      const photoRef = Firebase.storage().ref(`verificationPhoto/${photoGuid}.jpg`);
+      const addressRef = Firebase.storage().ref(`verificationProofOfAddress/${addressGuid}`);
+
+      photoRef.put(this.stepOneFile)
+        .then(() => addressRef.put(this.stepTwoFile))
+        .then(() => {
+          const ref = Firebase.database().ref();
+          const updates = {};
+          updates[`/drivers/${this.data.$id}/status`] = 'Verified';
+          ref.update(updates).then(() => {
+            this.$root.$emit('hide::modal', `${this.type}Modal`);
+          });
+        });
+    },
+    S4() {
+      // eslint-disable-next-line no-bitwise
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    },
+    getGuid() {
+      return `${this.S4()}${this.S4()}-${this.S4()}-4${this.S4().substr(0, 3)}-${this.S4()}-${this.S4()}${this.S4()}${this.S4()}`.toLowerCase();
     },
   },
 };
@@ -528,6 +614,7 @@ export default {
 .reference-nav-hidden {
   margin-right: 0px !important;
 }
+
 .btn-verify {
   margin-left: 10px;
   height: 20px !important;
@@ -536,7 +623,8 @@ export default {
   background-color: #4869a4 !important;
   border-color: #4869a4 !important;
   color: white;
-  font-size: 10px !important;
+  font-size: 9px !important;
+  font-weight: 600;
   transition: ease-out 0.2;
 }
 
@@ -544,6 +632,40 @@ export default {
   background-color: #094977 !important;
   border-color: #094977 !important;
   transition: ease-out 0.2;
+}
+
+.btn-browse {
+  height: 30px !important;
+  background-color: #4869a4 !important;
+  border-color: #4869a4 !important;
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+  margin-left: 10px;
+}
+
+.btn-browse:hover {
+  background-color: #094977 !important;
+  border-color: #094977 !important;
+}
+
+.btn-submit-verification {
+  margin-top: 20px;
+  width: 160px;
+  height: 30px !important;
+  background-color: #63a54b !important;
+  border-color: #63a54b !important;
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+}
+
+.btn-submit-verification:hover {
+  background-color: #83bd6e !important;
+  border-color: #83bd6e !important;
+}
+
+.name-file {
+  width: 250px;
+  font-size: 11px;
 }
 </style>
 
