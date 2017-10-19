7ae1fdc5dcbfcdf89c84f7b0e5b6712032e58483
qqc <qqc@dev.null>
2017-10-19 23:04:54 +0100

    Asd

---
+++ src/components/Chat.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="chat d-flex flex-column justify-content-start align-items-center">
+    <!-- <span class="txt-heading mr-auto">Manage Users</span> -->
+  </div>
+</template>
+
+<script>
+// import InviteUsersCard from '@/components/InviteUsersCard';
+
+export default {
+  name: 'Chat',
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+.chat {
+  width: calc(100% - 150px);
+  min-width: calc(100% - 150px);
+  /* background-color: #2c2e4a; */
+  background-color: #ececec;
+  padding: 20px;
+}
+</style>
