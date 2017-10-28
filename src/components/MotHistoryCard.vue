985f4d08bf323b81ca8036cf64eadd05856eff17
qqc <qqc@dev.null>
2017-10-28 02:24:32 +0100

    asd

--- src/components/MotHistoryCard.vue
+++ src/components/MotHistoryCard.vue
@@ -4,6 +4,11 @@
       <b-list-group class="history-list">
         <b-list-group-item class="history-list-item d-flex flex-column justify-content-start align-items-start" v-for="(mot, index) in motSubmissions" :key="mot.vehicleRegistration">
           <div class="data-row-container d-flex justify-content-start align-items-center">
+            <modal-data-row label="MOT Reference" :text="mot.motReference"></modal-data-row>
+            <modal-data-row label="MOT Start Date" :text="`${mot.motStartDate}, ${mot.motStartTime}`"></modal-data-row>
+            <modal-data-row label="MOT End Date" :text="`${mot.motEndDate}, ${mot.motEndTime}`"></modal-data-row>
+          </div>
+          <div class="data-row-container d-flex justify-content-start align-items-center">
             <modal-data-row label="Vehicle Registration" :text="mot.vehicleRegistration"></modal-data-row>
             <modal-data-row label="Manufacturer" :text="mot.manufacturer"></modal-data-row>
             <modal-data-row label="Model" :text="mot.model"></modal-data-row>
@@ -14,28 +19,15 @@
             <modal-data-row label="Country of Origin" :text="mot.countryOfOrigin"></modal-data-row>
           </div>
           <div class="data-row-container d-flex justify-content-start align-items-center">
-            <modal-data-row label="Engine Capacity" :text="mot.engineCapacity"></modal-data-row>
-            <modal-data-row label="Advisory Actions" :text="mot.advisoryActions"></modal-data-row>
-            <modal-data-row label="Current Odometer" :text="mot.currentOdometer"></modal-data-row>
-          </div>
-          <div class="data-row-container d-flex justify-content-start align-items-center">
             <modal-data-row label="Issuer Name" :text="mot.issuerName"></modal-data-row>
             <modal-data-row label="Issuing Authority" :text="mot.issuingAuthority"></modal-data-row>
             <modal-data-row label="Issuing Reference" :text="mot.issuingReference"></modal-data-row>
           </div>
           <div class="data-row-container d-flex justify-content-start align-items-center">
-            <modal-data-row label="MOT Start Date" :text="mot.motStartDate"></modal-data-row>
-            <modal-data-row label="MOT End Date" :text="mot.motEndDate"></modal-data-row>
-            <modal-data-row label="MOT Reference" :text="mot.motReference"></modal-data-row>
+            <modal-data-row label="Engine Capacity" :text="mot.engineCapacity"></modal-data-row>
+            <modal-data-row label="Advisory Actions" :text="mot.advisoryActions"></modal-data-row>
+            <modal-data-row label="Current Odometer" :text="mot.currentOdometer"></modal-data-row>
           </div>
-          <!-- <img class="img-avatar" src="../assets/user_avatar.jpg"></img>
-          {{user}}
-          <base-btn @click="clickEditUser(index)" class="ml-auto btn-view" icon="pencil"></base-btn>
-          <div class="d-flex flex-column">
-            <b-badge pill variant="success">ACCEPTED</b-badge>
-            <span class="txt-timeago">2 hours ago</span>
-          </div>
-          <edit-user-modal :user="user" :index="index"></edit-user-modal> -->
         </b-list-group-item>
       </b-list-group>
     </dark-card>
@@ -95,7 +87,7 @@ export default {
 .history-list {
   width: 100%;
   flex: 1;
-  max-height: 250px;
+  max-height: 200px;
   overflow-y: auto;
 }
 
@@ -105,6 +97,7 @@ export default {
   /* border-color: #8f90a8;
   color: white; */
   padding: 0.75rem 1rem;
+  border-radius: 4px;
 }
 
 .history-list-item:first-child {
